16fab9a9f95e66007f6440531bd5bf8ffd98b7d4
Sophie <sophie.zelinsky>
2024-09-05 11:42:24 +0200

    just notes

--- read_data.py
+++ read_data.py
@@ -13,9 +13,13 @@ for p in channel_name:
     ecogs.append(torch.tensor(data, dtype=torch.float32).squeeze())
 
 target_name = 'MOV_LEFT_CLEAN'
-target, time = raw[target_name, :] 
+target, time = raw[target_name, :]
+
+#MOV_LEFT_CLEAN contains 2 arrays: first one (target) with only zeros (using that, code runs okay), second one (time) produces only NAN loss after first epoch, dont rlly know what thats about
 
 x_data = torch.stack(ecogs, dim=1).unsqueeze(0) #just testing 4 now
 x_data = (x_data - x_data.mean()) / x_data.std()
 x_data = x_data.permute(0, 2, 1)
-y_data = torch.tensor(target.T, dtype=torch.float32).view(1, 1, 130001)
+y_data = torch.tensor(time.T, dtype=torch.float32).view(1, 1, 130001) 
+
+print(data)
