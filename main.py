59f17bf1423bb9db2684970b39c8cb83ec47eb2e
Sophie460 <sophie.zesy@gmail.com>
2024-09-06 12:06:37 +0200

    can continue training after pause now :)

--- main.py
+++ main.py
@@ -15,10 +15,20 @@ optimizer1 = torch.optim.SGD(model1.parameters(), lr=0.001)
 model2 = EEGCNNRegr()
 lossfunc2 = torch.nn.MSELoss()
 optimizer2 = torch.optim.SGD(model2.parameters(), lr=0.1)
+start_epoch = 0
+pretrained_model_path = "model_1000_epochs.pth"
+try:
+    breakpoint = torch.load(pretrained_model_path)
+    model2.load_state_dict(breakpoint["model_state_dict"])
+    optimizer2.load_state_dict(breakpoint["optimizer_state_dict"])
+    start_epoch = breakpoint["epoch"] + 1
+    print(f"starting training with epoch {start_epoch}")
+except FileNotFoundError:
+    print("starting training new")
 
 loss_values = []
 q = 1000
-for i in range(q): 
+for i in range(start_epoch, q): 
     pred_y = model2(x_data)
     loss = lossfunc2(pred_y, y_data)
 
@@ -30,7 +40,10 @@ for i in range(q):
     if i % 10 == 0:
         print('epoch {}, loss {}'.format(i, loss.item()))
 
-torch.save(model2.state_dict(), f"model_{q}_epochs.pth")
+torch.save({"epoch": i,
+            "model_state_dict": model2.state_dict(),
+            "optimizer_state_dict": optimizer2.state_dict(),
+            "loss": loss.item(),}, f"model_{q}_epochs.pth")
 
 #plt.plot(x_data, loss_values) "module" object is not callable
 #plt.xlabel("epochs")
