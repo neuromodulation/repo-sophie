be92c832e2c9017c1da50f388653b86f63dc3e6b
Sophie460 <sophie.zesy@gmail.com>
2024-09-16 13:06:53 +0200

    not a lot has changed, but now i am more annoyed lol

--- wav2vec2.py
+++ wav2vec2.py
@@ -2,7 +2,7 @@ import torch
 import torch.multiprocessing as mp
 from torch.utils.data import IterableDataset
 
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Trainer, TrainingArguments, Wav2Vec2Tokenizer
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Trainer, TrainingArguments, Wav2Vec2Tokenizer, DataCollatorWithPadding
 import soundfile as sf
 from datasets import load_dataset
 import librosa
@@ -11,6 +11,8 @@ import shutil
 import os
 import pickle
 
+
+#before starting the code, u have to $env:PYTHONUTF8="1" in Terminal (apparently only runs with UTF8)
 
 """
 def preprocess_function(batch, processor):
@@ -32,7 +34,7 @@ def preprocess_function(batch):
     batch["speech"] = speech
     return batch
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-
+data_collator = DataCollatorWithPadding(tokenizer)
 
 class IterableDATA(IterableDataset):
     def __init__(self, dataset, preprocess_function, processor):
@@ -57,39 +59,28 @@ def main():
         print("Cache has been cleared")
     else:
         pass
-    #
-    #before starting the code, u have to $env:PYTHONUTF8="1" in Terminal (apparently only runs with UTF8)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"trains on {device}")
 
-    try:
-        model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
-        model = model.to(device)
-    except (pickle.UnpicklingError, EOFError) as e:
-        print(f"failed to load model: {e}###########################################################")
-
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
+    model = model.to(device)
+    
 #gitignore: ganzen ordner? wegen transformern & rye container stuff
-#is now training on english, "ru" for russian, "de" for german
     dataset = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation", trust_remote_code=True) 
-    #try:
-    #    dataset = load_dataset("emotion", split="train[:10%]")
+    # dataset = load_dataset("emotion", split="train[:10%]")
     df = dataset.map(preprocess_function)
-    inputs = tokenizer(df["speech"][0], return_tensors="pt").input_values 
-    hidden_states = model(inputs).last_hidden_state
-    #except (pickle.UnpicklingError, EOFError) as e:
-    #    print(f"failed to load Dataset: {e}########################################################")
+    inputs = tokenizer(df["speech"], return_tensors="pt", padding=True).input_values.to(device)
+    # outputs = model(inputs)
+    logits = model(inputs).logits #logits statt hidden states, wenn man mit CTC arbeitet
+    #hidden_states = model(inputs).last_hidden_state
 
     #train_dataset, eval_dataset = train_test_split(df, test_size=0.2, random_state=42)
     #train_ds = dataset.from_pandas(train_dataset)
     #eval_ds = dataset.from_pandas(eval_dataset)
 
-    try:
-        processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h", trust_remote_code=True)
-    except (pickle.UnpicklingError, EOFError) as e:
-        print(f"failed to load processor: {e}###########################################################")
-
-#processed_dataset = dataset.map(preprocess_function)
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h", trust_remote_code=True)
+    #processed_dataset = dataset.map(preprocess_function)
     #train_data = IterableDATA(train_ds, preprocess_function, processor)
     #eval_data = IterableDATA(eval_ds, preprocess_function, processor)
 
@@ -110,12 +101,15 @@ def main():
         model=model,
         args=training_arg,
         train_dataset=inputs,
-        tokenizer=processor.feature_extractor
+        #tokenizer=tokenizer,
+        tokenizer=processor.feature_extractor,
+        data_collator=data_collator
     )
 
     trainer.train()
     trainer.evaluate()
     model.save_pretrained("./fine_tuned_wav2vec2")
+    predicted_ids = torch.argmax(logits, dim=-1)
 
 if __name__ == "__main__":
     # freeze_support()
