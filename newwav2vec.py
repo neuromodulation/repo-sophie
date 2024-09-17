0a207ffebbff95a70e7faffa7436db565deabc94
Sophie460 <sophie.zesy@gmail.com>
2024-09-17 17:19:20 +0200

    always throwing a torch.outOfMemory Error no matter what i do (testing example code rn)

--- newwav2vec.py
+++ newwav2vec.py
@@ -1,21 +1,75 @@
-from transformers import Wav2Vec2Config, Wav2Vec2Model, Wav2Vec2Tokenizer
-from datasets import load_dataset
+from transformers import Wav2Vec2Config, Wav2Vec2Model, Wav2Vec2Tokenizer, Wav2Vec2CTCTokenizer, Wav2Vec2FeatureExtractor, Wav2Vec2Processor
 import soundfile as sf
 import torch
+from datasets import load_dataset#, load_metric
+import json
+import re
 
-tokenizer = Wav2Vec2Tokenizer("facebook/wav2vec2-base-960h")
-model = Wav2Vec2Model("facebook/wav2vec2-base-960h")
+from datasets import ClassLabel
+import random
+import pandas as pd
+from IPython.display import display, HTML
 
-def preprocessing(batch):
-    speech, _ = sf.read(batch["file"])
-    batch["speech"] = speech
-    return batch
+# def show_random_elements(dataset, num_examples=10):
+#     assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
+#     picks = []
+#     for _ in range(num_examples):
+#         pick = random.randint(0, len(dataset)-1)
+#         while pick in picks:
+#             pick = random.randint(0, len(dataset)-1)
+#         picks.append(pick)
+    
+#     df = pd.DataFrame(dataset[picks])
+#     display(HTML(df.to_html()))
 
-ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
-ds = ds.map(map_to_array)
-input_values = tokenizer(ds["speech"][0], return_tensors="pt").input_values
-#hidden_states = model(input_values).last_hidden_state
-logits = model(input_values).logits
 
-predicted_ids = torch.argmax(logits, dim=-1)
-transcription = tokenizer.decode(predicted_ids[0])
+
+def main():
+
+    timit = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", trust_remote_code=True)
+    timit = timit.remove_columns(["file", "speaker_id", "id", "chapter_id"])
+    chars_to_ignore_regex = '[\,\?\.\!\-\;\:\"]'
+
+    def remove_special_characters(batch):
+        batch["text"] = re.sub(chars_to_ignore_regex, '', batch["text"]).lower()
+        return batch
+    timit = timit.map(remove_special_characters)
+
+    def extract_all_chars(batch):
+        all_text = " ".join(batch["text"])
+        vocab = list(set(all_text))
+        return {"vocab": [vocab], "all_text": [all_text]}
+
+
+
+    vocabs = timit.map(extract_all_chars, batched=True, batch_size=-1, keep_in_memory=True, remove_columns=timit.column_names["validation"])
+    vocab_list = list(set(vocabs["validation"]["vocab"][0]) | set(vocabs["validation"]["vocab"][0])) #eigentlich mit train & test
+    vocab_dict = {v: k for k, v in enumerate(vocab_list)}
+#print(vocab_dict)
+    vocab_dict["|"] = vocab_dict[" "]
+    del vocab_dict[" "]
+    vocab_dict["[UNK]"] = len(vocab_dict)
+    vocab_dict["[PAD]"] = len(vocab_dict)
+#30 tokens in dict (or 28 in our case), so liniear layer with output dimension 28 on top of pretrained
+
+    with open('vocab.json', 'w') as vocab_file:
+        json.dump(vocab_dict, vocab_file)
+
+    tokenizer = Wav2Vec2CTCTokenizer("./vocab.json", unk_token="[UNK]", pad_token="[PAD]", word_delimiter_token="|")
+
+#feature extractor:
+
+    feature_extractor = Wav2Vec2FeatureExtractor(feature_size=1, sampling_rate=16000, padding_value=0.0, do_normalize=True, return_attention_mask=False)
+    processor = Wav2Vec2Processor(feature_extractor=feature_extractor, tokenizer=tokenizer)
+
+
+    def speech_file_to_array_fn(batch):
+        speech_array, sampling_rate = sf.read(batch["file"])
+        batch["speech"] = speech_array
+        batch["sampling_rate"] = sampling_rate
+        batch["target_text"] = batch["text"]
+        return batch
+    timit = timit.map(speech_file_to_array_fn, num_proc=4)
+
+if __name__ == "__main__":
+    main()
--- wav2vec2.py
+++ wav2vec2.py
@@ -1,19 +1,24 @@
 import torch
-import torch.multiprocessing as mp
-from torch.utils.data import IterableDataset
+# import torch.multiprocessing as mp
+# from torch.utils.data import IterableDataset
 
-from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Trainer, TrainingArguments, Wav2Vec2Tokenizer, DataCollatorWithPadding
+from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC, Trainer, TrainingArguments, DataCollatorWithPadding, Wav2Vec2CTCTokenizer, Wav2Vec2Tokenizer
 import soundfile as sf
-from datasets import load_dataset
-import librosa
-from sklearn.model_selection import train_test_split
-import shutil
+from datasets import load_dataset#, gradient_checkpointing_enable
+# import librosa
+# from sklearn.model_selection import train_test_split
+# import shutil
 import os
-import pickle
+# import pickle
 
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
+#labeln mit videoaufnahmen, für das erkennen netz trainieren, mit generator & discriminator? 
 
+#Dataset is 7*73 large, but still torch.OutOfMemoryError
 #before starting the code, u have to $env:PYTHONUTF8="1" in Terminal (apparently only runs with UTF8)
+#maybe also $env:PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" for optimized training
 
+torch.cuda.empty_cache()
 """
 def preprocess_function(batch, processor):
     speech_array, sampling_rate = sf.read(batch["path"])
@@ -33,69 +38,54 @@ def preprocess_function(batch):
     speech, _ = sf.read(batch["file"])
     batch["speech"] = speech
     return batch
+
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
 data_collator = DataCollatorWithPadding(tokenizer)
 
-class IterableDATA(IterableDataset):
-    def __init__(self, dataset, preprocess_function, processor):
-        self.dataset = dataset
-        self.preprocess_function = preprocess_function
-        self.processor = processor
+# class IterableDATA(IterableDataset):
+#     def __init__(self, dataset, preprocess_function, processor):
+#         self.dataset = dataset
+#         self.preprocess_function = preprocess_function
+#         self.processor = processor
 
-    def __iter__(self):
-        for i in range(len(self.dataset)):
-            yield preprocess_function(self.dataset[i], self.processor)
+#     def __iter__(self):
+#         for i in range(len(self.dataset)):
+#             yield preprocess_function(self.dataset[i], self.processor)
 
-    def __len__(self):
-        return len(self.dataset)
+#     def __len__(self):
+#         return len(self.dataset)
         
 
 def main():
     #berlindaten auf 1d tensor runtersampeln
+    #torch.cuda.empty_cache()
 
-    cache = os.path.expanduser('~\\.cache\\huggingface\\transformers')
-    if os.path.exists(cache):
-        shutil.rmtree(cache)
-        print("Cache has been cleared")
-    else:
-        pass
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cpu")#torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"trains on {device}")
 
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-960h")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
     model = model.to(device)
+    #model.gradient_checkpointing_enable() #für bessere GPU Nutzung aus Huggingface
     
-#gitignore: ganzen ordner? wegen transformern & rye container stuff
     dataset = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation", trust_remote_code=True) 
-    # dataset = load_dataset("emotion", split="train[:10%]")
     df = dataset.map(preprocess_function)
     inputs = tokenizer(df["speech"], return_tensors="pt", padding=True).input_values.to(device)
-    # outputs = model(inputs)
     logits = model(inputs).logits #logits statt hidden states, wenn man mit CTC arbeitet
-    #hidden_states = model(inputs).last_hidden_state
 
-    #train_dataset, eval_dataset = train_test_split(df, test_size=0.2, random_state=42)
-    #train_ds = dataset.from_pandas(train_dataset)
-    #eval_ds = dataset.from_pandas(eval_dataset)
-
-    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-large-960h", trust_remote_code=True)
-    #processed_dataset = dataset.map(preprocess_function)
-    #train_data = IterableDATA(train_ds, preprocess_function, processor)
-    #eval_data = IterableDATA(eval_ds, preprocess_function, processor)
-
+    processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base-960h", trust_remote_code=True)
     training_arg = TrainingArguments(
         output_dir="./results",
         evaluation_strategy="epoch",
-        per_device_train_batch_size=8, ###
-        per_device_eval_batch_size=8,
+        per_device_train_batch_size=1, ###bei =8 bis =1 torch.outOfMemoryError
+        per_device_eval_batch_size=1, ##
         logging_dir="./logs",
         save_total_limit=2,
         save_steps=500,
-        fp16=False,
-        gradient_accumulation_steps=2,
-        dataloader_num_workers=0
-    )
+        fp16=True, ##half precision cause still torch.OutOfMemoryError
+        gradient_accumulation_steps=4, ##
+        dataloader_num_workers=0,
+        gradient_checkpointing=True, #for GPU memory, but slows down training by about 20%
+        optim="adafactor")   
 
     trainer = Trainer(
         model=model,
@@ -115,3 +105,5 @@ if __name__ == "__main__":
     # freeze_support()
     #mp.set_start_method("spawn")
     main()
+
+# print(torch.cuda.current_device()) (output: 0), but cuda is available
